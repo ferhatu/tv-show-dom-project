12beadda9328c13037f9f145d2e1d634aca282b9
ferhatu <ferhat_2001@yahoo.com>
2020-04-22 20:11:51 +0100

    update

--- script.js
+++ script.js
@@ -7,38 +7,43 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.innerHTML = `Got ${episodeList.length} episode(s)
+  rootElem.innerHTML = `Search ${episodeList.length} episode(s)
                         <input type="search" id="site-search">`; //Create search field
 
   //Create div for whole list of episodes
   let episodes = document.createElement("div");
   episodes.className = "episodeContainer";
-  let newEpisodes = episodeList
+
+  episodes.innerHTML = newEpisodes;
+  rootElem.appendChild(episodes);
+  // console.log(episodes);
+  // console.log(episodeList);
+  let inputField = document.querySelector("#site-search");
+  console.log(inputField.value);
+
+  inputField.addEventListener("keyup", function () {
+    let result = episodeList.filter(
+      (key) =>
+        key.summary.includes(inputField.value) ||
+        key.name.includes(inputField.value)
+    );
+    console.log(result);
+    return result;
+  });
+}
+function createNewList(episodeList) {
+  return episodeList
     .map(function (item) {
       return `<div class="episode">
             <h1 class="heading">${
               item.name
             } - S${item.season.toString().padStart(2, "0")}E${item.number.toString().padStart(2, "0")}</h1>
             <img class="image" src=${item.image.medium} alt= ${item.name}
-            <p class="parag">${item.summary}</p>
+            <p>${item.summary}</p>
             </div>`;
     })
     .join("");
-  episodes.innerHTML = newEpisodes;
-  rootElem.appendChild(episodes);
-  // console.log(episodes);
-  // console.log(episodeList);
-  let inputField = document.querySelector("#site-search");
-  inputField.addEventListener("keyup", function () {
-    const result = episodeList.filter((key) =>
-      (key.summary || key.name).includes(inputField.value)
-    );
-    // episodeList.filter((key) =>
-    //   (key.summary || key.name).includes(inputField.value)
-    return result;
-  });
 }
-
 window.onload = setup;
 
 // let inputField = document.createElement("input");
